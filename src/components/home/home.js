916f939d21be442c48fcb8af40e00bd63271874a
HowlDevs <157731912+HowlDevs@users.noreply.github.com>
2024-04-04 19:25:06 -0300

    commit aaaa
    asdasd

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -38,7 +38,7 @@ const Home = () => {
 
   return (
     <Container>
-<SearchBar/>
+<SearchBar/>AAAAA
       <Grid container spacing={2}>
         {dataPokemon.map(pokemon => (
           <Grid item key={pokemon.name} xs={12} lg={4}>
